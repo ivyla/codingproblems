4878ec5bba4d65536ecf6782f13f7ad2801ad43b
Ivy <ivyla3@gmail.com>
2021-06-02 13:47:59 -0700

    unique

--- stringsSingle.js
+++ stringsSingle.js
@@ -1,13 +1,23 @@
 let testStrings = []
 const testAlgorithm = (string) => {
     let result
-    console.log(`Before: ${string}`)
-    // console.log(")
-    result = "hi"
-    console.log(`Result: ${result} \n ----`)
+    // --------------------------------------
+    let reference = new Array(128)
+    for (let i = 0; i < string.length; i++) {
+        if (string.length > 128) return false
+        let currentChar = string[i]
+        if (reference[currentChar]) return false
+        reference[currentChar] = true
+    }
+    return true
+    // --------------------------------------
 }
 
 for (let i = 2; i < process.argv.length; i++) {
-    // testStrings.push(process.argv[i])
-    testAlgorithm(process.argv[i])
+    let currentString = process.argv[i]
+    console.log(`Testing: ${currentString}`)
+    let result = testAlgorithm(currentString)
+    console.log(`Result: ${result}`)
+    // console.log(`Result: ${result} \n ----`)
+    console.log(`----`)
 }
